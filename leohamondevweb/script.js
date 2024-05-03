32a307f33994fc172d46d8ca45a34a2f97499f51
Lemon Hao <164911131+lemonhao9@users.noreply.github.com>
2024-05-03 16:33:43 +0200

    Add files via upload
    Mise à jour avec quelques fixs

---
+++ leohamondevweb/script.js
@@ -0,0 +1,8 @@
+function toggle_text() {
+    var span = document.getElementById("span_txt");
+    if(span.innerHTML != "") {
+      span.innerHTML = "";
+    } else {
+      span.innerHTML = "0686618467";
+    }
+  }
